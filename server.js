1fa91f3990d7ef13e1bed2b96aedef5209cc29d6
NITHINSAI <149461948+NITHIN-SAI-SRM@users.noreply.github.com>
2025-04-20 00:43:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './config/config.env' });
 
+import express from 'express';
+import path from 'path';
 import app from './app.js';
+
+// Required when using ES modules (import)
+const __dirname = path.resolve();
+
+// Serve static files from the React frontend build folder
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
